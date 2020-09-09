488806cb429466dbd5bd480df79e510f1d5e6217
Gil Marsh <marsh.skc@gmail.com>
2020-09-08 19:47:57 -0500

    added Rocket class

---
+++ studio/Rocket.ts
@@ -0,0 +1,50 @@
+import { Cargo } from './Cargo';
+import { Astronaut } from './Astronaut';
+import { Payload } from './Payload';
+
+class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[];
+    astronauts: Astronaut[];
+
+    constructor(rocketName: string, rocketCapacity: number) {
+        this.name = rocketName;
+        this.totalCapacityKg = rocketCapacity;
+    }
+
+    sumMass( items: Payload[] ): number {
+        let sumItems: number;
+        for (let i: number = 0; i < items.length; i++) {
+            sumItems += items[i].massKg;
+        }
+        return sumItems;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
+    }
+
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        return false;
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    }
+}
